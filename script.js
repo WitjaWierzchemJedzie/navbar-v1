474e4dcfa8e35dbc61a2c87c4874f5ac709f44ce
WitjaWierzchemJedzie <witek.borowiec@sojournsolutions.com>
2022-04-11 15:37:22 +0200

    navigation-bar

---
+++ script.js
@@ -0,0 +1,5 @@
+const mobileButton = document.querySelector(".navigation-menu");
+const mobileMenu = document.querySelector(".navigation-links");
+mobileButton.addEventListener("click",function() {
+    mobileMenu.classList.toggle("d-block");
+});
